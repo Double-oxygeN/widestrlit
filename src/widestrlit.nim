be7b9d23c40be0ac2e9c167e71d997a254468c9d
Double-oxygeN <mail@double-oxygen.net>
2020-12-23 15:38:48 +0900

    naive implementations for `utf16`, `utf32`

--- src/widestrlit.nim
+++ src/widestrlit.nim
@@ -12,3 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import macros
+
+when defined(c) or defined(nimdoc):
+  {.passC: "-std=c11".}
+  type
+    Char16* {.importc: "char16_t", header: "<uchar.h>".} = distinct uint16
+    Char32* {.importc: "char32_t", header: "<uchar.h>".} = distinct uint32
+
+    Utf16String* {.importc: "char16_t *", header: "<uchar.h>".} = distinct ptr UncheckedArray[Char16]
+    Utf32String* {.importc: "char32_t *", header: "<uchar.h>".} = distinct ptr UncheckedArray[Char32]
+
+
+macro utf16*(strLit: string{lit}): untyped =
+  let
+    sym = genSym(nskConst, "str")
+    symStrLit = sym.toStrLit()
+  result = quote do:
+    (; {.emit: ["const char16_t *", `symStrLit`, r" = u""", `strLit`, "\";"].}; let p {.importc: `symStrLit`, nodecl.}: Utf16String; p)
+
+
+macro utf32*(strLit: string{lit}): untyped =
+  let
+    sym = genSym(nskConst, "str")
+    symStrLit = sym.toStrLit()
+  result = quote do:
+    (; {.emit: ["const char32_t *", `symStrLit`, r" = U""", `strLit`, "\";"].}; let p {.importc: `symStrLit`, nodecl.}: Utf32String; p)
+
+
+when isMainModule:
+  expandMacros:
+    let
+      p0 = utf16"Hello!"
+      p1 = utf16"こんにちは!"
+      p2 = utf32"Hello!"
+      p3 = utf32"😀"
+
+    echo repr(p0)
+    echo repr(p1)
+    echo repr(p2)
+    echo repr(p3)
